1b006f326f1d3d5000190724494f901921b5b1da
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-05-22 13:25:57 -0300

    using find method to get any argument inside of a URL pattern string

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
 print(url)
 
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
-
 url_base = url[:indice_interrogacao]
-
-print(url_base)
-
 url_parametros = url[indice_interrogacao+1:]
-
 print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = "moedaDestino"
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find("&", indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
