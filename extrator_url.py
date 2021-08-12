ec1a9eef05fc0924f1189d9c8d157c02139fbfa2
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-08-12 19:27:48 -0300

    Use regex to validate URL

--- extrator_url.py
+++ extrator_url.py
@@ -1,3 +1,6 @@
+import re
+
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -12,6 +15,12 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+
+        padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+        match = padrao_url.match(url)
+
+        if not match:
+            raise ValueError("A URL não é válida")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find("?")
@@ -33,6 +42,8 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
-extrator_url = ExtratorURL(None)
-valor_quantidade = extrator_url.get_valor_parametro("moedaDestino")
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
---
+++ validador_url.py
@@ -0,0 +1,12 @@
+# https://www.bytebank.com.br/cambio
+
+import re
+
+url = "www.bytebank.com.br/cambio"
+padrao_url = re.compile("(http(s)?://)?(www.)?bytebank.com(.br)?/cambio")
+match = padrao_url.match(url)
+
+if not match:
+    raise ValueError("A URL não é válida")
+
+print("A URL é válida")
