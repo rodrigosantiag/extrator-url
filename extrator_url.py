06b7a21d302a868eb46f675023645ccce2ee0d79
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-08-15 19:32:28 -0300

    Implement eq method in ExtratorURL class

--- extrator_url.py
+++ extrator_url.py
@@ -48,11 +48,17 @@ class ExtratorURL:
     def __str__(self):
         return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
 
+    def __eq__(self, other):
+        return self.url == other.url
+
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
+extrator_url2 = ExtratorURL(url)
 print("O tamanho da URL: ", len(extrator_url))
 print(extrator_url)
+
+print(extrator_url == extrator_url2)
 
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
